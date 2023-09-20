208c14c941ad12d22a230a60b3d2e31b328c0855
kqakqakqa <58761476+kqakqakqa@users.noreply.github.com>
2023-09-21 00:17:44 +0800

    Add files via upload
    Retry when fail (eg. no Internet connection), for at most 12 times * 5 seconds.

--- wallpaper-only-once.py
+++ wallpaper-only-once.py
@@ -1,26 +1,39 @@
 import ctypes
 import json
 import os
+import time
 import urllib.request
 
-# get url
-archive = urllib.request.urlopen("https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN").read()
-archiveJson = json.loads(archive)
-imageUrl = "https://www.bing.com" + archiveJson["images"][0]["url"]
-imageUrl = imageUrl.replace("1920x1080", "UHD")
 
-# get path
-imagePath = os.path.dirname(os.path.abspath(__file__)) + "/bg.jpg"
-if os.path.exists(imagePath):
-    os.remove(imagePath)
+def wallpaper():
+    # get url
+    archive = urllib.request.urlopen("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN").read()
+    archiveJson = json.loads(archive)
+    imageUrl = "https://www.bing.com" + archiveJson["images"][0]["url"]
+    imageUrl = imageUrl.replace("1920x1080", "UHD")
 
-# download
-urllib.request.urlretrieve(imageUrl, imagePath)
+    # get path
+    imagePath = os.path.dirname(os.path.abspath(__file__)) + "/bg.jpg"
+    if os.path.exists(imagePath):
+        os.remove(imagePath)
 
-# set
-SPI_SETDESKWALLPAPER = 0x0014
-SPIF_UPDATEINIFILE = 0x0001
-SPIF_SENDWININICHANGE = 0x0002
-ctypes.windll.user32.SystemParametersInfoW(
-    SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE
-)
+    # download
+    urllib.request.urlretrieve(imageUrl, imagePath)
+
+    # set
+    SPI_SETDESKWALLPAPER = 0x0014
+    SPIF_UPDATEINIFILE = 0x0001
+    SPIF_SENDWININICHANGE = 0x0002
+    ctypes.windll.user32.SystemParametersInfoW(
+        SPI_SETDESKWALLPAPER, 0, imagePath, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE
+    )
+
+
+trialLeft = 12
+while trialLeft > 0:
+    try:
+        wallpaper()
+        break
+    except:
+        trialLeft -= 1
+        time.sleep(5)
