23623dc7c84e4be253e3d27d90c62809e9eca474
kqakqakqa <58761476+kqakqakqa@users.noreply.github.com>
2023-10-19 00:47:25 +0800

    wallpaper.jpg will be left

--- wallpaper-only-once.py
+++ wallpaper-only-once.py
@@ -3,24 +3,27 @@ import json
 import os
 import time
 import urllib.request
+import sys
 
 
 def wallpaper():
-    # get url
+    print("getting url:")
     archive = urllib.request.urlopen("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN").read()
     archiveJson = json.loads(archive)
     imageUrl = "https://www.bing.com" + archiveJson["images"][0]["url"]
     imageUrl = imageUrl.replace("1920x1080", "UHD")
+    print(imageUrl + "\n")
 
-    # get path
-    imagePath = os.path.dirname(os.path.abspath(__file__)) + "/bg.jpg"
+    print("getting path:")
+    imagePath = os.path.dirname(os.path.realpath(sys.argv[0])) + "\\wallpaper.jpg"
     if os.path.exists(imagePath):
         os.remove(imagePath)
+    print(imagePath + "\n")
 
-    # download
+    print("downloading")
     urllib.request.urlretrieve(imageUrl, imagePath)
 
-    # set
+    print("setting")
     SPI_SETDESKWALLPAPER = 0x0014
     SPIF_UPDATEINIFILE = 0x0001
     SPIF_SENDWININICHANGE = 0x0002
@@ -33,7 +36,9 @@ trialLeft = 12
 while trialLeft > 0:
     try:
         wallpaper()
+        print("done")
         break
     except:
+        print("retrying")
         trialLeft -= 1
         time.sleep(5)
